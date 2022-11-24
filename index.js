605b6e45793bfec515bdd1af02ee7f43a3477804
zahed <zahediiuc221@gmail.com>
2022-11-24 19:33:32 +0600

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middle ware
 app.use(cors());
@@ -12,6 +13,38 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("laptop sell center server is running");
 });
+
+// Database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t4uwg.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  const usersCollection = client.db("anItBari").collection("users");
+  const categoriesCollection = client.db("anItBari").collection("categories");
+
+  try {
+    // users
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      const result = await usersCollection.insertOne(users);
+      res.send(result);
+    });
+
+    // categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoriesCollection.find(query).toArray();
+      res.send(categories);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.dir(error));
 
 app.listen(port, () => {
   console.log(`server is running at ${port}`);
